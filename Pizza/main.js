6a89fcae1aca33009d44a21364cc8d5f56ef8ad7
VivaanRawal <80296406+VivaanRawal@users.noreply.github.com>
2021-06-21 16:52:16 -0700

    Add files via upload

---
+++ Pizza/main.js
@@ -0,0 +1,7 @@
+var pizza=["Cheese Pizzaa", "Hawain Style", "Pepperoni "];
+function Pizza(){
+    name1=document.getElementById("student1").value;
+    pizza.push(name1);
+document.getElementById("show").innerHTML=pizza;
+
+}
